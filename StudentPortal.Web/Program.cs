9d1e1ab24ce9f0a7276785d81a85987816a6f86b
ehsan-nadernezhad <ehsannadernezhad2019@gmail.com>
2024-09-24 13:22:58 +0200

    Add CRUD in project.

--- StudentPortal.Web/Program.cs
+++ StudentPortal.Web/Program.cs
@@ -24,6 +24,7 @@ namespace StudentPortal.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
